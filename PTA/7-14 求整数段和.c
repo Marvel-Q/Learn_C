1d2ef263af796c2b45687c6ffda0cf47500d17e3
奇迹 <a1198135727@gmail.com>
2025-10-26 23:04:19 +0800

    Learning

--- PTA/7-14 求整数段和.c
+++ PTA/7-14 求整数段和.c
@@ -1,23 +1,23 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	int a, b,sum=0,count=0;
-//	scanf("%d %d", &a, &b);
-//	for (int i = a; i <= b; i++)
-//	{
-//		printf("%5d", i);
-//		count++;
-//		if (count%5==0)
-//		{
-//			printf("\n");
-//		}
-//		sum += i;
-//	}
-//	if (count%5!=0)
-//	{
-//		printf("\n");
-//	}
-//	printf("Sum = %d", sum);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a, b,sum=0,count=0;
+	scanf("%d %d", &a, &b);
+	for (int i = a; i <= b; i++)
+	{
+		printf("%5d", i);
+		count++;
+		if (count%5==0)
+		{
+			printf("\n");
+		}
+		sum += i;
+	}
+	if (count%5!=0)
+	{
+		printf("\n");
+	}
+	printf("Sum = %d", sum);
+	return 0;
+}
