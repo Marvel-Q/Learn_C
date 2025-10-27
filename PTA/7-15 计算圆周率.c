e99d16a2edf89389d0d52d443bba8e5992c7c0a2
奇迹 <a1198135727@gmail.com>
2025-10-27 23:26:48 +0800

    10.27

--- PTA/7-15 计算圆周率.c
+++ PTA/7-15 计算圆周率.c
@@ -2,6 +2,22 @@
 #include<stdio.h>
 int main()
 {
-
+	int a=1, b=1;
+	double item = 1;
+	double n;
+	double sum = 0;
+	scanf("%lf", &n);
+	while (item>=n)
+	{
+		item = item * a / b;
+		if (item!=1)
+		{
+			a++;
+		}
+		b += 2;
+		sum += item;
+	}
+	double pi = sum * 2;
+	printf("%.6f", pi);
 	return 0;
 }
